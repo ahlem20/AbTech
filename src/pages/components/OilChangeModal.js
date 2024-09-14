f8628a31b36c74ecac6af40b93347126ac2a3e04
ahlem20 <107472826+ahlem20@users.noreply.github.com>
2024-09-14 15:48:13 +0100

    Update OilChangeModal.js

--- src/pages/components/OilChangeModal.js
+++ src/pages/components/OilChangeModal.js
@@ -16,7 +16,7 @@ function AddOilTransaction({ closePopup }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://abtechapi.onrender.com/api/products/sellOil', formData);
+      const response = await axios.post('https://abtech-api2.onrender.com/api/products/sellOil', formData);
       alert(response.data.message);
       closePopup();
     } catch (error) {
